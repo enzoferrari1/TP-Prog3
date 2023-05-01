8e45015ff47690e96c03851b2606d049e3f93d99
enzoferrari1 <enzonob39@gmail.com>
2023-05-01 14:13:20 -0300

    Tres primeros algoritmos(bfs, dfs, ucs). Tuve problemas ya que el codigo termina prematuramente cuando la grilla es vacía, le puse contador al while ya que al parecer nunca termina, le preguntaré luego a los profes.

---
+++ Enzo/bfs.py
@@ -0,0 +1,63 @@
+from ..models.grid import Grid
+from ..models.frontier import QueueFrontier
+from ..models.solution import NoSolution, Solution
+from ..models.node import Node
+
+class BreadthFirstSearch:
+    @staticmethod
+    def search(grid: Grid) -> Solution:
+        """Find path between two points in a grid using Breadth First Search
+
+        Args:
+            grid (Grid): Grid of points
+
+        Returns:
+            Solution: Solution found
+        """
+        # Initialize a node with the initial position
+        node = Node("", grid.start, 0)
+
+        # Initialize the explored dictionary to be empty
+        explored = {}
+
+        # Add the node to the explored dictionary
+        explored[node.state] = True
+
+        # Create the frontier (FIFO policy, use queue)
+        frontier = QueueFrontier()
+
+        # Add first node to the frontier
+        frontier.add(node)
+
+        # Set maximum number of iterations to prevent infinite looping for debugging
+        max_iterations = 40000
+        iteration_count = 0
+
+        while iteration_count < max_iterations:
+            # Check if maximum number of iterations has been reached
+            if frontier.is_empty():
+                return NoSolution(explored)
+                break
+
+            # Proceed to take a node from the frontier according to the corresponding policy. Here, we first take the oldest nodes, which is why we use the queue structure
+            node = frontier.remove()
+            explored[node.state] = True
+
+            if node.state == grid.end:
+                return Solution(node, explored)
+                break
+
+            # Add adjacent cells to the frontier
+            neighbours = grid.get_neighbours(node.state)
+            for direction in neighbours.keys():
+                new_state = neighbours[direction]
+                new_node = Node("", new_state, node.cost + grid.get_cost(new_state))
+                new_node.parent = node
+                new_node.action = direction
+                if not explored.get(new_state, False):
+                    frontier.add(new_node)
+
+            iteration_count += 1
+
+        return NoSolution(explored)
+
---
+++ Enzo/ucs.py
@@ -0,0 +1,49 @@
+from ..models.grid import Grid
+from ..models.frontier import StackFrontier, PriorityQueueFrontier
+from ..models.solution import NoSolution, Solution
+from ..models.node import Node
+
+
+class UniformCostSearch:
+    @staticmethod
+    def search(grid: Grid) -> Solution:
+        """Find path between two points in a grid using Uniform Cost Search
+
+        Args:
+            grid (Grid): Grid of points
+
+        Returns:
+            Solution: Solution found
+        """
+        # Initialize a node with the initial position
+        node = Node("", grid.start, 0)
+
+        # Initialize the explored dictionary to be empty
+        explored = {} 
+        
+        # Add the node to the explored dictionary
+        explored[node.state] = True
+        frontier = PriorityQueueFrontier()
+        frontier.add(node)
+        counter = 0
+        max_counter = 20000
+        while counter < max_counter:
+          if frontier.is_empty():
+            return NoSolution(explored)
+            break
+          node = frontier.pop()
+          explored[node.state] = True
+          if node.state == grid.end:
+            return Solution(node, explored)
+            break
+
+          neighbours = grid.get_neighbours(node.state)
+          for direction in neighbours.keys():
+            new_state = neighbours[direction]
+            if not explored.get(new_state, False):
+              new_node = Node('', new_state, node.cost + grid.get_cost(new_state))
+              new_node.parent = node
+              new_node.action = direction
+              frontier.add(new_node, priority=grid.get_cost(new_state))
+          counter = counter + 1
+        return NoSolution(explored)
